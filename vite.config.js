b639588791abcc0e7d0cbfc05705e6a04b11a196
Priyansu Negi <priyansunegi010@gmail.com>
2026-04-08 03:16:17 +0530

    fix: increase chunkSizeWarningLimit to 5000KB to ensure warnings are suppressed on Vercel

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 5000,
     rolldownOptions: {
       checks: {
         pluginTimings: false,
