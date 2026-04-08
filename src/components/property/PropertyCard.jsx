bd3f8bc75b98841d86243ab337ca9546a902fab0
Priyansu Negi <priyansunegi010@gmail.com>
2026-04-09 03:21:24 +0530

    style: reduce bottom rounding on property cards and sync landlord dashboard layout

--- src/components/property/PropertyCard.jsx
+++ src/components/property/PropertyCard.jsx
@@ -60,7 +60,7 @@ const PropertyCardComponent = ({ property, layout = 'grid' }) => {
          onClick={() => navigate(`/property/${property.id}`)}
       >
         <div className="relative w-40 h-full flex-shrink-0 bg-gray-50">
-          <img src={mainImage} className="w-full h-full object-contain" />
+          <img src={mainImage} className="w-full h-full object-cover" />
         </div>
         <div className="py-4 pr-4 flex flex-col justify-between flex-1">
           <div>
@@ -94,8 +94,8 @@ const PropertyCardComponent = ({ property, layout = 'grid' }) => {
       )}
       onClick={() => navigate(`/property/${property.id}`)}
     >
-      {/* Image Container - Bottom edges rounded more than card */}
-      <div className="relative w-full aspect-video sm:aspect-video bg-gray-50 isolate rounded-b-2xl overflow-hidden p-1">
+      {/* Image Container */}
+      <div className="relative w-full aspect-[4/3] sm:aspect-[4/3] bg-gray-50 isolate rounded-b-md overflow-hidden">
         {!imgLoaded && !imgError && (
           <div className="skeleton absolute inset-0 z-0" />
         )}
@@ -104,7 +104,7 @@ const PropertyCardComponent = ({ property, layout = 'grid' }) => {
             src={mainImage}
             alt={property.title}
             className={cn(
-              'w-full h-full object-contain group-hover:scale-105 transition-transform duration-700',
+              'w-full h-full object-cover group-hover:scale-105 transition-transform duration-700',
               imgLoaded ? 'opacity-100' : 'opacity-0'
             )}
             loading="lazy"
